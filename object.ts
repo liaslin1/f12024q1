f786605895006c7fb2155906498515d8d03d37cb
My Name <myemail@example.com>
2024-10-11 11:12:43 +0300

    add: object car

---
+++ object.ts
@@ -0,0 +1,11 @@
+const machineCharacteristic = {
+    salon: 'темный натуральная кожа',
+    securitySystems: 'антипробуксовочная сигнализация иммобилайзер',
+    cushions: 'передние боковые задние',
+    aidSystems: 'датчик дождя камера заднего вида парктроники коньроль мертвых зон на зеркалах',
+    exterior: 'легкосплавные диски',
+}
+
+machineCharacteristic.salon = 'светлая натуральная кожа!!!!!!!'
+
+console.log(machineCharacteristic.cushions) 
---
+++ varible.ts
@@ -0,0 +1,5 @@
+const first_number = 2
+const second_number= 3
+const result=first_number+second_number
+
+console.log(result)
