bf9e4cc76d324706bc358b201d5b4692e3a2d5ed
My Name <myemail@example.com>
2024-10-18 11:39:16 +0300

    add:array.ts

---
+++ array.ts
@@ -0,0 +1,9 @@
+const fruits= ['яблоко', 'банан' ,'виноград', 'манго' ,'мандарин'];
+ const vagitables =[ 'огурец ' ,'помидор','капуста' ,'картошка' ,'тыква'  ];
+ fruits.push ('mashina')
+  const plants = fruits.concat(vagitables)
+  plants.sort()
+ console.log(plants)
+
+
+
