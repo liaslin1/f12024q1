bc65e1389911d572611e67a34e997a72f607bc3c
My Name <myemail@example.com>
2024-10-25 11:41:39 +0300

    add:arraycars

---
+++ Forarray.ts
@@ -0,0 +1,11 @@
+const arraycars=['havaldargo','toyota','bmwm8','niva','mercedes-benz' ]
+for(let y=0; y < arraycars.length; y++){
+    if(arraycars[y] ===' havaldargo'){
+        console.log(`в данном масиве найден ${arraycars [y]}`)
+    
+    }
+}
+
+
+arraycars[0]
+
