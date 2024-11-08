7ab207297b0ee5891559bb9aa8e1228a82b18691
My Name <myemail@example.com>
2024-11-08 11:08:39 +0300

    add: function

---
+++ functionsum.js
@@ -0,0 +1,22 @@
+function sum(num1, num2,num3) {
+    return num1 + num2 +num3
+
+}
+// console.log(sum(37,763,75))
+
+
+
+function allnumberssum ( ...params){
+    return params[2]
+}
+
+console.log (allnumberssum(1,5,7))
+
+function lol(...params){
+let num =S
+for (i of params){
+    num = i + num
+}
+return num
+}
+console.log (lol(1,2,3,4,5,6,8,9,10))
---
+++ musicFunction.js
@@ -0,0 +1,7 @@
+const musictons =(
+    Cdur ['do','re','mi','fa','sol','la','si'],
+    ddur ['re','mi','fa dies','sol','la','si','do dies'],
+)
+function t53(tons){
+    const tresvuchie =[]
+}
