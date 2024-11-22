27a23cd89a8291beef10e1a72968a71aa1adc20f
My Name <myemail@example.com>
2024-11-22 11:40:54 +0300

    add: words

---
+++ sort.js
@@ -0,0 +1,7 @@
+const words=['гитара', 'фортепиано', 'машина' ,'книга' ,'наушники','телефон' ,'кот','чай', 'кофта', 'дом']
+ 
+const resultDown = words.sort((a,b)=>a-b)
+resultDown.reverse()
+console.log(resultDown)
+
+
